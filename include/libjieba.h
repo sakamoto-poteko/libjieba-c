7f6216aec1b5f29f2157ac3bbfe118099764e84d
Afa Cheng <afa@afa.moe>
2019-12-04 17:55:19 +0800

    updated interface for all cut API

--- include/libjieba.h
+++ include/libjieba.h
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #else
 #include <stdlib.h>
+#include <stdbool.h>
 #endif
 
 #if defined(_WIN32)
@@ -23,38 +24,53 @@
 
 extern "C" {
 
-typedef void *jieba_context;
+	typedef void* jieba_context;
 
-typedef struct jieba_word_t {
-	char* word;
-	unsigned int len;
-} jieba_word;
+	typedef struct jieba_words_t {
+		char** words;
+		unsigned int count;
+	} jieba_words;
+
+	typedef struct jieba_compact_word_t {
+		const char* word;	// start
+		unsigned int len;
+	} jieba_compact_word;
+
+	typedef struct jieba_compact_words {
+		jieba_compact_word *words;
+		unsigned int count;
+	} jieba_compact_words;
 	
-typedef struct jieba_words_t {
-    jieba_word *words;
-    unsigned int count;
-	int allocated;
-} jieba_words;
+	typedef struct jieba_tag_t {
+		char* word;
+		char* tag;
+	} jieba_tag;
 
-LIBJIEBA_API jieba_context jieba_create_context(const char *dict_path, const char *hmm_path, const char *user_dict, const char *idf_path, const char *stop_words_path);
-
-LIBJIEBA_API void jieba_destroy_context(jieba_context ctx);
-
-/// sentence: UTF-8
-LIBJIEBA_API int jieba_cut(jieba_context ctx, const char* sentence, jieba_words* words, int hmm);
-
-LIBJIEBA_API int jieba_cut_full(jieba_context ctx, const char *sentence, unsigned int len, int allocate_words, jieba_words *words, int hmm);
-
-LIBJIEBA_API int jieba_cut_all(jieba_context ctx, const char* sentence, jieba_words* words);
-
-LIBJIEBA_API int jieba_cut_all_full(jieba_context ctx, const char* sentence, unsigned int len, int allocate_words, jieba_words* words);
-
-LIBJIEBA_API int jieba_cut_for_search(jieba_context ctx, const char* sentence, jieba_words* words, int hmm);
-
-LIBJIEBA_API int jieba_cut_for_search_full(jieba_context ctx, const char* sentence, unsigned int len, int allocate_words, jieba_words* words, int hmm);
+	typedef struct jieba_tags_t {
+		jieba_tag *tags;
+		unsigned int count;
+		char* tag_list;
+	} jieba_tags;
 	
-LIBJIEBA_API void jieba_words_free(jieba_words *w);
-	
+	LIBJIEBA_API jieba_context jieba_create_context(const char* dict_path, const char* hmm_path, const char* user_dict, const char* idf_path, const char* stop_words_path);
+	LIBJIEBA_API void jieba_destroy_context(jieba_context ctx);
+	LIBJIEBA_API void jieba_words_free(jieba_words* w);
+	LIBJIEBA_API void jieba_compact_words_free(jieba_compact_words* w);
+	LIBJIEBA_API void jieba_tags_free(jieba_tags* t);
+	LIBJIEBA_API bool jieba_insert_user_word(jieba_context ctx, const char* word);
+	LIBJIEBA_API bool jieba_insert_user_word_with_tag(jieba_context ctx, const char* word, const char* tag);
+
+
+	LIBJIEBA_API int jieba_cut(jieba_context ctx, const char* sentence, jieba_words* words, bool hmm);
+	LIBJIEBA_API int jieba_cut_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words, bool hmm);
+	LIBJIEBA_API int jieba_cut_full_compact(jieba_context ctx, const char* sentence, unsigned int len, jieba_compact_words* words, bool hmm);
+	LIBJIEBA_API int jieba_cut_all(jieba_context ctx, const char* sentence, jieba_words* words);
+	LIBJIEBA_API int jieba_cut_all_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words);
+	LIBJIEBA_API int jieba_cut_all_full_compact(jieba_context ctx, const char* sentence, unsigned int len, jieba_compact_words* words);
+	LIBJIEBA_API int jieba_cut_for_search(jieba_context ctx, const char* sentence, jieba_words* words, bool hmm);
+	LIBJIEBA_API int jieba_cut_for_search_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words, bool hmm);
+	LIBJIEBA_API int jieba_cut_for_search_full_compact(jieba_context ctx, const char* sentence, unsigned int len, jieba_compact_words* words, bool hmm);
+
 }
 
 #endif // LIBJIEBA_H
--- src/libjieba.cpp
+++ src/libjieba.cpp
@@ -18,109 +18,259 @@
 #include "cppjieba/Jieba.hpp"
 
 
+// Local decl
 static int string_vector_to_jieba_words(const std::vector<std::string>& input, jieba_words* words);
+static cppjieba::Jieba* ctx2jieba(jieba_context ctx);
 
-jieba_context jieba_create_context(const char* dict_path, const char* hmm_path, const char* user_dict, const char* idf_path, const char* stop_words_path)
-{
-	jieba_context ctx = static_cast<jieba_context>(new cppjieba::Jieba(dict_path, hmm_path, user_dict, idf_path, stop_words_path));
-	return ctx;
-}
-
-void jieba_destroy_context(jieba_context ctx)
-{
-	delete static_cast<cppjieba::Jieba*>(ctx);
-}
-
+// Local functions
 static int string_vector_to_jieba_words(const std::vector<std::string>& input, jieba_words* words)
 {
 	words->words = nullptr;
-	words->allocated = false;
 	words->count = 0;
 
 	if (!input.empty()) {
 		words->count = input.size();
 
 		// 1. allocate jieba_word array
-		words->words = (jieba_word*)malloc(sizeof(jieba_word) * input.size());
+		words->words = (char**)malloc(sizeof(char*) * input.size());
 		if (words->words == nullptr)
 		{
 			return 0;
 		}
 
-		// 2. mark words as allocated
-		words->allocated = 1;
-
-		// 3. populate jieba_word array
+		// 2. populate jieba_word array
 		for (unsigned i = 0; i < input.size(); ++i) {
 			const auto& w = input[i];
-			const auto word = words->words + i;
-			word->word = strdup(w.c_str());	// Can be w.data() if on C++11/later
-			word->len = w.size();
+			words->words[i] = strdup(w.c_str());	// Can be w.data() if on C++11/later
 		}
 	}
 
 	return input.size();
 }
 
-int jieba_cut(jieba_context ctx, const char* sentence, jieba_words* words, int hmm)
+static int string_vector_to_jieba_compact_words(const std::vector<std::string> &input, jieba_compact_words *words, const char *original)
 {
-	return jieba_cut_full(ctx, sentence, std::strlen(sentence), true, words, hmm);
+	words->words = nullptr;
+	words->count = 0;
+
+	if (!input.empty())
+	{
+		words->words = (jieba_compact_word *)malloc(sizeof(jieba_compact_word) * input.size());
+		if (words->words == nullptr)
+		{
+			return 0;
+		}
+
+		for (unsigned i = 0, offset = 0; i < input.size(); ++i)
+		{
+			jieba_compact_word* w = words->words + i;
+			w->word = original + offset;
+			const auto len = input[i].size();
+			w->len = len;
+			offset += len;
+		}
+	}
+
+	return input.size();
 }
 
-int jieba_cut_full(jieba_context ctx, const char* sentence, unsigned int len, int allocate_words, jieba_words* words, int hmm)
+static cppjieba::Jieba* ctx2jieba(jieba_context ctx)
 {
-	const auto jieba = static_cast<cppjieba::Jieba*>(ctx);
+	return static_cast<cppjieba::Jieba*>(ctx);
+}
 
-	std::vector<std::string> result;
-	const std::string s(sentence, len);
+// Jieba obj & utils
 
-	jieba->Cut(s, result, hmm);
+jieba_context jieba_create_context(const char* dict_path, const char* hmm_path, const char* user_dict, const char* idf_path, const char* stop_words_path)
+{
+	const jieba_context ctx = static_cast<jieba_context>(new cppjieba::Jieba(dict_path, hmm_path, user_dict, idf_path, stop_words_path));
+	return ctx;
+}
 
-	return string_vector_to_jieba_words(result, words);
+void jieba_destroy_context(jieba_context ctx)
+{
+	delete ctx2jieba(ctx);
 }
 
 void jieba_words_free(jieba_words* w)
 {
-	if (w && w->allocated && w->words) {
+	if (w && w->words) {
 		for (unsigned i = 0; i < w->count; ++i) {
-			free(w->words[i].word);
+			free(w->words[i]);
 		}
 		free(w->words);
 	}
 }
 
+void jieba_compact_words_free(jieba_compact_words* w)
+{
+	if (w->words)
+	{
+		free(w->words);
+	}
+}
+
+void jieba_tags_free(jieba_tags* t)
+{
+	if (!t) {
+		return;
+	}
+
+	if (t->tag_list)
+	{
+		free(t->tag_list);
+	}
+
+	if (t->tags)
+	{
+		for (unsigned i = 0; i < t->count; ++i)
+		{
+			free(t->tags[i].word);
+		}
+
+		free(t->tags);
+	}
+}
+
+bool jieba_insert_user_word(jieba_context ctx, const char* word)
+{
+	return ctx2jieba(ctx)->InsertUserWord(word);
+}
+
+bool jieba_insert_user_word_with_tag(jieba_context ctx, const char* word, const char* tag)
+{
+	return ctx2jieba(ctx)->InsertUserWord(word, tag);
+}
+
+// Cut
+
+#define CUTF_SIGNATURE jieba_context ctx, const std::string& s, std::vector<std::string>& result, bool hmm
+
+template <typename cutF>
+int generic_cut_jieba_words(jieba_context ctx, const char* sentence, unsigned int len, jieba_words *output, bool hmm, cutF fnCut)
+{
+	std::vector<std::string> result;
+	const std::string s(sentence, len);
+
+	fnCut(ctx, s, result, hmm);
+
+	return string_vector_to_jieba_words(result, output);
+}
+
+template <typename cutF>
+int generic_cut_jieba_compact_words(jieba_context ctx, const char* sentence, unsigned int len, jieba_compact_words* output, bool hmm, cutF fnCut)
+{
+	std::vector<std::string> result;
+	const std::string s(sentence, len);
+
+	fnCut(ctx, s, result, hmm);
+
+	return string_vector_to_jieba_compact_words(result, output, sentence);
+}
+
+int jieba_cut(jieba_context ctx, const char* sentence, jieba_words* words, bool hmm)
+{
+	return generic_cut_jieba_words(ctx, sentence, std::strlen(sentence), words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->Cut(s, result, hmm);
+		});
+}
+
+int jieba_cut_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words, bool hmm)
+{
+	return generic_cut_jieba_words(ctx, sentence, len, words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->Cut(s, result, hmm);
+		});
+}
+
+int jieba_cut_full_compact(jieba_context ctx, const char* sentence, unsigned int len, jieba_compact_words* words, bool hmm)
+{
+	return generic_cut_jieba_compact_words(ctx, sentence, len, words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->Cut(s, result, hmm);
+		});
+}
+
 int jieba_cut_all(jieba_context ctx, const char* sentence, jieba_words* words)
 {
-	return jieba_cut_all_full(ctx, sentence, std::strlen(sentence), true, words);
+	return generic_cut_jieba_words(ctx, sentence, std::strlen(sentence), words, false,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutAll(s, result);
+		});
 }
 
-int jieba_cut_all_full(jieba_context ctx, const char* sentence, unsigned  int len, int allocate_words,
-	jieba_words* words)
+int jieba_cut_all_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words)
 {
-	const auto jieba = static_cast<cppjieba::Jieba*>(ctx);
-
-	std::vector<std::string> result;
-	const std::string s(sentence, len);
-
-	jieba->CutAll(s, result);
-
-	return string_vector_to_jieba_words(result, words);
+	return generic_cut_jieba_words(ctx, sentence, len, words, false,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutAll(s, result);
+		});
 }
 
-int jieba_cut_for_search(jieba_context ctx, const char* sentence, jieba_words* words, int hmm)
+int jieba_cut_all_full_compact(jieba_context ctx, const char* sentence, unsigned len, jieba_compact_words* words)
 {
-	return jieba_cut_for_search_full(ctx, sentence, std::strlen(sentence), true, words, hmm);
+	return generic_cut_jieba_compact_words(ctx, sentence, len, words, false,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutAll(s, result);
+		});
 }
 
-int jieba_cut_for_search_full(jieba_context ctx, const char* sentence, unsigned int len, int allocate_words,
-	jieba_words* words, int hmm)
+
+int jieba_cut_for_search(jieba_context ctx, const char* sentence, jieba_words* words, bool hmm)
 {
-	const auto jieba = static_cast<cppjieba::Jieba*>(ctx);
-
-	std::vector<std::string> result;
-	const std::string s(sentence, len);
-
-	jieba->CutForSearch(s, result, hmm);
-
-	return string_vector_to_jieba_words(result, words);
+	return generic_cut_jieba_words(ctx, sentence, std::strlen(sentence), words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutForSearch(s, result, hmm);
+		});
 }
+
+int jieba_cut_for_search_full(jieba_context ctx, const char* sentence, unsigned int len, jieba_words* words, bool hmm)
+{
+	return generic_cut_jieba_words(ctx, sentence, len, words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutForSearch(s, result, hmm);
+		});
+}
+
+int jieba_cut_for_search_full_compact(jieba_context ctx, const char* sentence, unsigned len, jieba_compact_words* words,
+	bool hmm)
+{
+	return generic_cut_jieba_compact_words(ctx, sentence, len, words, hmm,
+		[](CUTF_SIGNATURE)
+		{
+			const auto jieba = ctx2jieba(ctx);
+			jieba->CutForSearch(s, result, hmm);
+		});
+}
+
+// Cut & Tag
+
+//int jieba_cut_with_tag(jieba_context ctx, const char* sentence)
+//{
+//	const auto jieba = ctx2jieba(ctx);
+//
+//	std::vector<std::pair<std::string, std::string>> tags;
+//	jieba->Tag(sentence, tags);
+//
+//	std::map<std::string, unsigned int> tag_list_map;
+//
+//}
+
+
